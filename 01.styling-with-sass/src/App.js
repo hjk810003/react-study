2301a392ff334b705c52b856a6cd34bdd584686e
hyunjung <hjk810003@gmail.com>
2023-01-19 17:13:18 +0900

    scss props 설정 및 rest props 전달

--- 01.styling-with-sass/src/App.js
+++ 01.styling-with-sass/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Button from './components/Button';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='buttons'>
+        <Button size='large'>BUTTON</Button>
+        <Button>BUTTON</Button>
+        <Button size='small'>BUTTON</Button>
+      </div>
+      <div className='buttons'>
+        <Button color='pink' size='large'>BUTTON</Button>
+        <Button color='pink'>BUTTON</Button>
+        <Button color='pink' size='small'>BUTTON</Button>
+      </div>
+      <div className='buttons'>
+        <Button color='gray' size='large'>BUTTON</Button>
+        <Button color='gray'>BUTTON</Button>
+        <Button color='gray' size='small'>BUTTON</Button>
+      </div>
+      <div className='buttons'>
+        <Button size='large' outline>BUTTON</Button>
+        <Button color='pink' outline>BUTTON</Button>
+        <Button color='gray' size='small' outline>BUTTON</Button>
+      </div>
+      <div className='buttons'>
+        <Button size='large' fullWidth>BUTTON</Button>
+        <Button size='large' color='pink' fullWidth>BUTTON</Button>
+        <Button 
+          size='large' 
+          color='gray' 
+          fullWidth
+          onClick={() => {
+            console.log('클릭!');
+          }}
+          onMouseMove={() => {
+            console.log('마우스무브!');
+          }}
+        >BUTTON</Button>
+      </div>
     </div>
   );
 }
--- 01.styling-with-sass/src/components/Button.js
+++ 01.styling-with-sass/src/components/Button.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import classNames from 'classnames';
+import './Button.scss';
 
-function Button() {
+// size: large, medium, small
+// color: blue, pink, gray
+function Button({
+    children, 
+    size, 
+    color, 
+    outline, 
+    fullWidth, 
+    className,
+    ...rest
+}) {
+    // return <button className={['Button', size].join('')}>{children}</button>
+    // return <button className={`Button ${size}`}>{children}</button>
     return (
-        <div>
-
-        </div>
+        <button 
+            className={classNames('Button', size, color, {
+            outline, 
+            fullWidth
+        },
+        className
+        )}
+        {...rest}
+        >
+            {children}
+        </button>
     )
 }
+
+Button.defaultProps = {
+    size: 'medium',
+    color: 'blue'
+};
 
 export default Button;
